ec25e6452fd9d9c04829ba64c16cd3de26e902be
GervasioEl-Vasco <khoiripaldi16@gmail.com>
2026-09-01 10:42:38 +0700

    Pertemuan 1

---
+++ app/src/main/java/com/example/jualan/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.jualan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jualan.ui.theme.JualanTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JualanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LayoutTentangJualan()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutTentangJualan() {
+    JualanTheme {
+        Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Tentang Jualan")
+            
+        }
+    }
+}
